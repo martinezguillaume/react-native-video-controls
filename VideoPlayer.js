0fa5b4987239d5a5a7ff7688eef32d44bf1f1fca
Guizmo <martinezguillaum@gmail.com>
2018-02-28 16:02:30 +0100

    Add onToggleFullscreen props

--- VideoPlayer.js
+++ VideoPlayer.js
@@ -421,6 +421,8 @@ export default class VideoPlayer extends Component {
         // state.resizeMode = state.isFullscreen === true ? 'cover' : 'contain';
 
         // this.setState( state );
+        const { onToggleFullscreen } = this.props
+        onToggleFullscreen && onToggleFullscreen(state.isFullscreen)
         this.player.ref.presentFullscreenPlayer()
     }
 
